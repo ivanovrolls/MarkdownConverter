c1348ffedc563c660c9e4dd6883f2bd8d76dd827
Ivan Germanoff <ivanovrolls@Ivans-MacBook-Air.local>
2025-07-18 10:23:14 +0100

    Attempt to remove old or renamed files

--- index.py
+++
@@ -1,9 +0,0 @@
-import markdown
-
-with open('sample.md', 'r') as f:
-    markdown_string = f.read()
-
-html_string = markdown.markdown(markdown_string)
-
-with open('sample.html', 'w') as f:
-    f.write(html_string)
